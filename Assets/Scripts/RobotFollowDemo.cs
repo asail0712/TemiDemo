edd6b1c7217f1cc3dbe759fd8388fcf9b4e2ab8c
GrandenCorp <info@granden.rocks>
2025-09-11 16:36:35 +0800

    [Add]新增跟隨與回到充電站功能

---
+++ Assets/Scripts/RobotFollowDemo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Granden.temi;
+
+public class RobotFollowDemo : MonoBehaviour
+{
+    [SerializeField] private Button followMeBtn;
+    [SerializeField] private Button stopFollowBtn;
+    [SerializeField] private Button backToBtn;
+
+    private bool bRobotReady        = false;
+
+    private TemiRobotProxy robotProxy;
+
+    void Start()
+    {
+        robotProxy = new TemiRobotProxy();
+
+#if UNITY_ANDROID// && !UNITY_EDITOR
+        robotProxy.InitialProxy((isReady) =>
+        {
+            bRobotReady = isReady;
+
+            // 綁定按鈕
+            if (followMeBtn)
+            {
+                followMeBtn.onClick.AddListener(() => 
+                { 
+                    Debug.Log("[Temi] Follow Me");
+                    robotProxy.Call("beWithMe", (AndroidJavaObject)null);
+                });
+            }
+
+            if (stopFollowBtn)
+            {
+                stopFollowBtn.onClick.AddListener(() => 
+                { 
+                    Debug.Log("[Temi] Stop Following");
+                    robotProxy.Call("stopMovement");
+                });
+            }
+
+            if (backToBtn)
+            {
+                backToBtn.onClick.AddListener(() =>
+                {
+                    Debug.Log("[Temi] Back To Battery");
+                    robotProxy.Call("goTo", "home base");
+                });
+            }
+        });
+#else
+        Debug.Log("[TemiSpeakDemo] 僅在 Android 裝置上執行（Editor 已略過 JNI）");
+#endif
+    }
+
+    void OnDestroy()
+    {
+#if UNITY_ANDROID
+        if(robotProxy == null)
+        {
+            return;
+        }
+
+        robotProxy.ReleaseProxy();
+#endif
+    }
+}
